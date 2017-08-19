0c8f75101836cdcce2c6e4085b6025e900284636
Jeremias Wolff <jeremiaswolff@gmail.com>
2017-08-19 22:04:05 +0200

    build navigation component in vue

---
+++ resources/assets/js/components/NavbarLinks.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li>
+            <router-link :to="{ name: 'home'}">
+                <i class="glyphicon glyphicon-home"></i>
+            </router-link>
+        </li>
+        <router-link :to="{ name: 'product-list'}" tag="li">
+            <a>Produkte</a>
+        </router-link>
+        <router-link :to="{ name: 'catalog-list'}" tag="li">
+            <a>Katalog</a>
+        </router-link>
+    </ul>
+</template>
