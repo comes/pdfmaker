2da4ff66d899c45b39b50fac4c89344de6201a80
Jeremias Wolff <jeremiaswolff@gmail.com>
2017-08-19 22:04:58 +0200

    modify main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,7 @@
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                    &nbsp;<navbar-links class="nav navbar-nav"></navbar-links>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
